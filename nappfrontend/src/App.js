602e2ccf05e2dbddfa1524fca7a2e463e064e07c
AdegokeJaafar <jaafaradegoke@gmail.com>
2021-08-10 12:35:28 +0100

    header name added

---
+++ nappfrontend/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>News Web App</h1>
+        <p>By Jaafar</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
